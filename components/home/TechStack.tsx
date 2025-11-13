186af57fae9b04ce4ae134e7fa4288b0f729bd29
aldinpramudya <aldinarielpramudya@gmail.com>
2025-11-13 11:55:12 +0700

    feat : techStack fixing

--- components/home/TechStack.tsx
+++ components/home/TechStack.tsx
@@ -2,8 +2,21 @@
 import { Separator } from "@/components/ui/separator";
 // Components
 import TechStackCard from "@/components/TechStackCard";
-// Constants
-import { techStackData } from "@/constants";
+// Icons
+import { RiNpmjsFill } from "react-icons/ri";
+import { SiTypescript } from "react-icons/si";
+import { FaLaravel } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiPnpm } from "react-icons/si";
+
+import { GrMysql } from "react-icons/gr";
+import { SiMongodb } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+
+import { SiNextdotjs } from "react-icons/si";
+import { FaPhp } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+
 
 export default function TechStack() {
     return (
@@ -13,14 +26,27 @@ export default function TechStack() {
                     tech stack
                 </p>
                 <Separator className="my-4" />
-                <div className="space-y-3">
-                    {techStackData.map((data) => (
-                        <TechStackCard key={data.id}>
-                            <p>
-                                {data.text}
-                            </p>
-                        </TechStackCard>
-                    ))}
+                <div className="space-y-3 flex flex-col">
+                    <TechStackCard>
+                        <p className="font-bold flex flex-row items-center">
+                           <FaPhp/>&nbsp;PHP+&nbsp;<SiTypescript/>&nbsp;Typescript+&nbsp;<IoLogoJavascript/>&nbsp;Javascript 
+                        </p>
+                    </TechStackCard>
+                    <TechStackCard>
+                        <p className="font-bold flex flex-row items-center">
+                            <RiNpmjsFill/>&nbsp;NPM +&nbsp;<SiPnpm/>&nbsp;PNPM 
+                        </p>
+                    </TechStackCard>
+                    <TechStackCard>
+                        <p className="font-bold flex flex-row items-center">
+                            <FaLaravel/>&nbsp;Laravel +&nbsp;<SiNextdotjs/>&nbsp;Next.js+&nbsp;<FaReact/>&nbsp;React
+                        </p>
+                    </TechStackCard>
+                    <TechStackCard>
+                        <p className="font-bold flex flex-row items-center">
+                            <GrMysql/>&nbsp;MySQL +&nbsp;<SiMongodb/>&nbsp;MongoDB + &nbsp;<SiPostgresql/>&nbsp;PostgreSQL
+                        </p>
+                    </TechStackCard>
                 </div>
             </div>
         </>
